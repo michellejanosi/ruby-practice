86b863ec94e083d6fa3ffa4c98d8a97243adaf40
michellejanosi <michelle.janosi@gmail.com>
2017-02-12 13:47:53 -0500

    Added challenges folder and solution to first exercise

---
+++ basics/runaway_loop.rb
@@ -0,0 +1,12 @@
+# Modify the code so the loop stops after the first iteration.
+
+#original code
+loop do
+  puts 'Just keep printing...'
+end
+
+#modified code
+loop do
+  puts 'Just keep printing...'
+  break
+end
