f9470571f25ee11c77df5ef3d208176e6207a182
michellejanosi <michelle.janosi@gmail.com>
2017-04-14 16:18:59 -0400

    Added solutions to exercises

---
+++ basics/greet_your_friends.rb
@@ -0,0 +1,6 @@
+# Your friends just showed up! Given the following array of names, use a for loop
+# to greet each friend individually.
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+friends.each { |friend| puts "Hello, #{friend}." }
