b982f4ca0c93f3329653c68654f4a48b41e862d9
michellejanosi <michelle.janosi@gmail.com>
2017-04-16 09:09:57 -0400

    Added exercise solutions

---
+++ basics/only_even.rb
@@ -0,0 +1,16 @@
+# Using next, modify the code below so that it only prints even numbers.
+
+number = 0
+
+until number == 10
+  number += 1
+  puts number
+end
+
+#modified
+number = 0
+
+until number == 10
+  number += 1
+  puts number.next if number.odd?
+end
