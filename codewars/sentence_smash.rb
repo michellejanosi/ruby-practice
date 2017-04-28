7bec2c5146087e989bb942b6de74b556cd906c24
michellejanosi <michelle.janosi@gmail.com>
2017-04-28 10:39:44 -0400

    Added exercise solution

---
+++ codewars/sentence_smash.rb
@@ -0,0 +1,11 @@
+# Write a method smash that takes an array of words and smashes them together into
+# a sentence and returns the sentence. You can ignore any need to sanitize words or
+# add punctuation, but you should add spaces between each word. Be careful, there
+# shouldn't be a space at the beginning or the end of the sentence!
+
+# Smash Words
+def smash(words)
+  words.join(" ")
+end
+
+smash(["hello", "world"]) # => "hello world"
