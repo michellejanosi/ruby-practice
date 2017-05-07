6909d77d7f38b3f4a5192f80bf476489249bd9eb
michellejanosi <michelle.janosi@gmail.com>
2017-05-07 11:43:24 -0400

    Added exercise solution

---
+++ small-problems/array_average.rb
@@ -0,0 +1,12 @@
+# Write a method that takes one argument, an array containing integers, and returns
+# the average of all numbers in the array. The array will never be empty and the
+# numbers will always be positive integers.
+
+# The tests should print true.
+
+def average(arr)
+  arr.reduce(:+) / arr.length
+end
+
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
