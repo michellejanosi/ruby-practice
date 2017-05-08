38d970c488715bc74e3d8ea4c496fea8a0600ea1
michellejanosi <michelle.janosi@gmail.com>
2017-05-08 08:12:18 -0400

    Added exercise solution

---
+++ small-problems/how_old_is_teddy.rb
@@ -0,0 +1,10 @@
+# Build a program that randomly generates and prints Teddy's age. To get the age,
+# you should generate a random number between 20 and 200.
+
+# Example Output:
+#
+# Teddy is 69 years old!
+
+def age
+  puts "Teddy is #{rand(20...200)} years old!"
+end
